3d1a5e84f7df5ebb3bfa3bbcb73c118cebc09d4a
Jsamson1997 <158376245+Jsamson1997@users.noreply.github.com>
2024-10-16 16:06:44 -0700

    Add files via upload
    File Manager

---
+++ File Manager.py
@@ -0,0 +1,33 @@
+''' 
+File management
+
+This should create a folder within the selected folder with
+results 
+
+ '''
+import os 
+
+class FileManager:
+    
+    #Need to create folder object 
+    
+    # Our Folder location as our attribute
+    # Get the directory of the current file (Python file must be in folder with images)
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    print("Current directory:", current_directory)
+    
+    
+    #Folder methods 
+    
+    #Creates folder 
+    #Note: Needs object 
+    def CreateFolder(self):
+        if not os.path.exists("path/to/demo_folder"): 
+              os.makedirs("path/to/demo_folder")
+              
+              
+              
+if __name__ == '__main__':
+    print(CreateFolder())
+
+
