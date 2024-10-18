933a85e861c6b64673cfc027154ccc04a5738e32
Joshua Samson <923053622@sfsu.edu>
2024-10-17 22:37:18 -0700

    Psedocode

--- File Manager.py
+++ File Manager.py
@@ -17,13 +17,59 @@ class FileManager:
     print("Current directory:", current_directory)
     
     
-    #Folder methods 
+#Folder methods 
     
     #Creates folder 
     #Note: Needs object 
     def CreateFolder(self):
         if not os.path.exists("path/to/demo_folder"): 
               os.makedirs("path/to/demo_folder")
+
+    #Deletes folder 
+        #Get directory 
+        #Select folder 
+        #Deletes folder 
+
+    #Change Folder Location
+        #Get directory of current folder
+        #Get directory of selected folder 
+        
+
+#File methods
+
+    #Create File
+        #Get Folder directory
+            #Conditional for checking if file is created
+            #False if not there
+            #True if there
+        #Creates file 
+
+    #Load file 
+        #Get Folder directory 
+        #Get File name 
+        #Load File 
+
+    #Copy Files
+        #Get Folder directory 
+        #Get File name 
+        #Copy file date 
+
+    #Delete File 
+        #Get Folder directory 
+        #Get File name 
+        #Copy file date 
+
+    #Move File
+        #Get Folder directory 
+        #Get File name 
+        #Copy file date 
+
+    #Change File Location
+        #Get directory of current folder
+        #Get selected file 
+        #Get directory of selected folder
+        #Copy File
+        #Paste File 
               
               
               
